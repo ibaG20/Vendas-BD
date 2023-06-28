e1116bc8194cffddb9dde8f4e949b0a7d996d662
MisterAqua <italohyu122@gmail.com>
2023-06-27 23:30:09 -0300

    PostVendasADD

--- bd-back/controllers/vendas.js
+++ bd-back/controllers/vendas.js
@@ -31,37 +31,53 @@ export const getVendas = async (_, res) => {
 };
 
 //------------------------------------------------
-export const postVendas = (req, res) => {
+// Função para lidar com o post de vendas
+export const postVendas = async (req, res) => {
   const { ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo, itens } = req.body;
 
-  const insertVendaSql = "INSERT INTO tb_vendas(ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo) VALUES ($1, $2, $3, $4)";
-  const vendaValues = [ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo];
+  // Cria um novo cliente
+  const client = new Client(db);
 
-  db.query(insertVendaSql, vendaValues, (err, result) => {
-    if (err) {
-      db.end(); // Feche a conexão em caso de erro
-      return res.json(err);
-    }
+  try {
+    // Conecta o cliente
+    await client.connect();
 
-    const vendaId = vendaValues[0]; // Obtém o ID da venda inserida
+    // Inicia uma transação
+    await client.query('BEGIN');
 
-    const insertItensSql = "INSERT INTO tb_itens(ite_codigo, ite_quantidade, ite_valor_parcial, tb_produtos_pro_codigo, tb_vendas_ven_codigo) VALUES ($1, $2, $3, $4, $5)";
+    // Insere os dados da venda na tabela tb_vendas
+    const vendaQuery = {
+      text: 'INSERT INTO tb_vendas (ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo) VALUES ($1, $2, $3, $4) RETURNING *',
+      values: [ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo]
+    };
 
-    const itensValues = itens.map(item => [
-      item.ite_codigo,
-      item.ite_quantidade,
-      item.ite_valor_parcial,
-      item.tb_produtos_pro_codigo,
-      vendaId
-    ]);
+    const vendaResult = await client.query(vendaQuery);
+    const vendaId = vendaResult.rows[0].ven_codigo;
+  
+    for (let i = 0; i < itens.length; i++){
+    // Insere os itens da venda na tabela tb_itens_venda
+    let itensQuery = {
+      text: 'INSERT INTO tb_itens (ite_codigo, ite_quantidade, ite_valor_parcial, tb_produtos_pro_codigo, tb_vendas_ven_codigo) VALUES ($1, $2, $3, $4, $5)',
+      values: [itens[i].ite_codigo, itens[i].ite_quantidade, itens[i].ite_valor_parcial, itens[i].tb_produtos_pro_codigo, vendaId]
+    };
 
-    db.query(insertItensSql, itensValues, (err, result) => {
-      db.end(); // Feche a conexão após a conclusão das consultas
+    await client.query(itensQuery);
 
-      if (err) return res.json(err);
+    // Confirma a transação
+    await client.query('COMMIT');
 
-      return res.status(200).json("Venda e itens inseridos com sucesso!");
-    });
-  });
+    // Retorna uma resposta de sucesso
+    return res.status(200).json({ message: 'Venda criada com sucesso!' });
+  }
+  } catch (err) {
+    // Desfaz a transação em caso de erro
+    await client.query('ROLLBACK');
+
+    // Retorna o erro
+    return res.status(500).json({ error: err.message });
+  } finally {
+    // Independentemente do que acontecer, fecha a conexão
+    await client.end();
+  }
 };
 //------------------------------------------------
