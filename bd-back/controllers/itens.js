8c85d82ae552597b6cc15ca4919743012b2208ee
MisterAqua <italohyu122@gmail.com>
2023-06-29 19:48:59 -0300

    saiComment

--- bd-back/controllers/itens.js
+++ bd-back/controllers/itens.js
@@ -1,36 +1,3 @@
-/* import { db } from "../db.js";
-import pkg from 'pg';
-const { Client } = pkg;
-
-export const getItens = async (_, res) => {
-    const q = "SELECT * FROM tb_itens";
-
-    // Cria um novo cliente
-    const client = new Client(db);
-
-    try {
-        // Conecta o cliente
-        await client.connect();
-
-        // Realiza a consulta
-        const result = await client.query(q);
-
-        // Responde com os dados
-        if (!result.rows.length) {
-            return res.status(200).json('vazio');
-        } else {
-            return res.status(200).json(result.rows);
-        }
-    } catch (err) {
-        // Se houver um erro, responde com o erro
-        return res.json(err);
-    } finally {
-        // Independentemente do que acontecer, fecha a conexão
-        await client.end();
-    }
-};
- */
-
 import pkg from 'pg';
 import jwt from 'jsonwebtoken';
 const { Client } = pkg;
